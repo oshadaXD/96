f0dd4a952572f05f803c60429d717b3c6d7441b8
Vimukthi_Oshada <108446553+oshadaXD@users.noreply.github.com>
2022-07-15 01:12:19 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,55 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+	
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '92170802cb1a',
+	
+}
+
+// Other
+global.owner = ['94775792013']
+global.premium = ['94775792013']
+global.packname =  process.env.PACKNAME ||'⛧ S A T H A N ⸸'
+global.author = '⛧ S A T H A N ⸸'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.themeemoji = "🦄"
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.visoka = { url: 'https://telegra.ph/file/39c0aba89c1281605b7de.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
